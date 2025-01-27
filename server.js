c8110762da0cdd604fe9b959903506626765fb68
HERCULES0000 <anuragbachheti1999@gmail.com>
2025-01-27 16:08:10 +0530

    changes made

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
+const dbUri = process.env.DB_URI;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
